8469545975091e23a090b917621ad99f3f5dc407
Fedor <screenshot@inbox.ru>
2020-10-29 23:19:38 +0700

    Complete second task

--- index.js
+++ index.js
@@ -1,4 +1,6 @@
-// sum(2,3) == sum(2)(3) == 5
+//1 TASK: sum(2,3) == sum(2)(3) == 5
+
+//First solution (unc if need to test)
 
 // function sum(a, b) {
 //   if (b) {
@@ -10,14 +12,41 @@
 //   };
 // }
 
-function sum(...args) {
-  if (args.length === 2) {
-    return args[0] + args[1];
-  }
-  return function (b) {
-    return args[0] + b;
-  };
-}
+//Second solution (unc if need to test)
 
-console.log(sum(2, 3));
-console.log(sum(2)(3));
+// function sum(...args) {
+//   if (args.length === 2) {
+//     return args[0] + args[1];
+//   }
+//   return function (b) {
+//     return args[0] + b;
+//   };
+// }
+
+//Unc if need to test:
+
+// console.log(sum(2, 3));
+// console.log(sum(2)(3));
+
+//===========================================
+
+//2 TASK
+
+/*
+const sum = (x,y) => x + y;
+const plusTen = sum.bind2(undefined, 10);
+plusTen(1) //11
+plusTen(10); //20
+*/
+
+const sum = (x, y) => x + y;
+
+Function.prototype.bind2 = function (context, ...args) {
+  return (...args2) => {
+    return this.apply(context, args.concat(args2));
+  };
+};
+
+const plusTen = sum.bind2(undefined, 10);
+console.log(plusTen(1)); //11
+console.log(plusTen(10)); //20
